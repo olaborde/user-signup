203e7bb480e999b0d3aa71a638e6b2805af22e0c
olaborde <oml007@live.com>
2018-10-10 17:50:15 -0400

    added route to signup

---
+++ routes.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+@app.route("/signup")
+def about():
+  return render_template("user-signup.html")
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
