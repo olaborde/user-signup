36b36317986e57997905aae4f26b983d1dcf7de2
olaborde <oml007@live.com>
2018-10-11 22:03:24 -0400

    password match validation added

--- forms.py
+++ forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, validators
+from wtforms.validators import DataRequired, Length
 class signupForm(Form):
     user_name = StringField('user_name', validators=[DataRequired("Please enter a username")])
-    password = PasswordField('password', validators=[DataRequired()])
-    password2 = PasswordField('password2',validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired(), Length(min=3, message="password should be between 3 and 20 characteres"), validators.EqualTo('vpassword', message='password mnust match')])
+    vpassword = PasswordField('vpassword',validators=[DataRequired(), Length(min=3, message="password should be between 3 and 20 characteres")])
     email = StringField('email')
     submit = SubmitField('signup')
+
--- routes.py
+++ routes.py
@@ -13,10 +13,12 @@ def index():
 def signup():
  
   form = signupForm() 
+  
   if request.method == 'POST':
       if form.validate == False:
         return render_template("user-signup.html", form=form)
       else:
+                
         return 'Welcome, ' + request.form['user_name']    
   elif request.method == 'GET':   
     return render_template("user-signup.html", form=form)
