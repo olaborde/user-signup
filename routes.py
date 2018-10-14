6745f4c31e70097ba809997995c6c8df95a51f69
olaborde <oml007@live.com>
2018-10-14 16:07:31 -0400

    save changes in the routes

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,flash, redirect, url_for
 from forms import signupForm
 
 app = Flask(__name__)
@@ -11,6 +11,9 @@ def index():
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
+  error = None
+  # password = request.form.get['password']
+  # vpassword = request.form.get['vpassword']
  
   form = signupForm() 
   
@@ -18,10 +21,14 @@ def signup():
       if form.validate == False:
         return render_template("user-signup.html", form=form)
       else:
-                
-        return 'Welcome, ' + request.form['user_name']    
+            if request.form['password'] != request.form['vpassword']:
+                  # error = 'Password must match'
+                  flash('Password must match!')
+                  return redirect(url_for('signup'))
+            else:    
+              return 'Welcome, ' + request.form['user_name']    
   elif request.method == 'GET':   
-    return render_template("user-signup.html", form=form)
+    return render_template("user-signup.html", form=form, error=error)
 
 
 if __name__ == "__main__":
