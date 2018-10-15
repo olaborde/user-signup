abb4766484d4bea77b799e27665210f83731a134
olaborde <oml007@live.com>
2018-10-14 23:17:50 -0400

    form logic

--- forms.py
+++ forms.py
@@ -1,17 +1,19 @@
 from flask_wtf import Form
+from flask import Flask, flash
 from wtforms import StringField, PasswordField, SubmitField, validators
 from wtforms.validators import DataRequired, Length, InputRequired
+from wtforms.fields.html5 import EmailField
 class signupForm(Form):
     user_name = StringField('user_name', validators=[DataRequired("Please enter a username")])
     password = PasswordField('password', validators=[DataRequired(), Length(min=3, message="password should be between 3 and 20 characteres"), validators.EqualTo('vpassword', message='password mnust match')])
     vpassword = PasswordField('vpassword',validators=[DataRequired(), Length(min=3, message="password should be between 3 and 20 characteres")])
-    email = StringField('email')
+    email = EmailField('email', validators=[EmailField])
     submit = SubmitField('signup')
 
+    # if len(email) == 0:
+    #     flash('Emal!')
+    #     return redirect(url_for('signup'))
 
-    name = StringField('user_name', [InputRequired()])
 
-    def validate_name(form, field):
-        if len(field.data) < 0:
-            raise ValidationError('username must between 3 or 20 characters')
+
 
